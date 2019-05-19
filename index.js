129de8df3c8d5c683bfccd92cc086bcc1e22a01f
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-19 16:10:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(location) {
-  return location - 42;
+  return Math.abs(location - 42);
 }
 
 function distanceFromHqInFeet(location) {
