a8b4fa27cf7a2bf309ec512c32388dca02a700fd
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-19 16:05:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(dest) {
+  return;
+}
+
+function distanceFromHqInFeet(dest) {
+  return distanceFromHqInBlocks * 264;
+}
+
+function distanceTravelledInFeet() {
+  return;
+}
+
+function calculatesFarePrice(start, destination) {
+  return;
+}
